d475631a1b35680f8074b418e0e945f1f855f729
Petr <kamaeff.petya@gmail.com>
2018-12-16 18:51:11 +0200

    Patch 1.0

--- app/src/main/java/deusvult/petrkamaev/homework_3/TriangleActivity.kt
+++ app/src/main/java/deusvult/petrkamaev/homework_3/TriangleActivity.kt
@@ -2,21 +2,25 @@ package deusvult.petrkamaev.homework_3
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import kotlinx.android.synthetic.main.activity_triangle.*
 import kotlin.math.roundToInt
 
 class TriangleActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_triangle)
 
         makeAResultBTNTriangle.setOnClickListener {
             if(s1EditText.text.isNotEmpty() && s2EditText.text.isNotEmpty() && s3EditText.text.isNotEmpty()){
-                var s1 = s1EditText.text.trim().toString().toInt()
-                var s2 = s2EditText.text.trim().toString().toInt()
-                var s3 = s3EditText.text.trim().toString().toInt()
-                mainTextViewTriangle.text = "Square is ~ " + TriangleClass(s1, s2, s3).S().roundToInt() + "\n Perimeter is: " + TriangleClass(s1, s2, s3).P().toString()
+                var s1 = s1EditText.text.trim().toString().toDouble()
+                var s2 = s2EditText.text.trim().toString().toDouble()
+                var s3 = s3EditText.text.trim().toString().toDouble()
+                if(s1 < (s2+s3) && s2 < (s1+s3) && s3 < (s1+s2)){
+                    mainTextViewTriangle.text = "Square is ~ " + TriangleClass(s1, s2, s3).S().roundToInt() + "\n Perimeter is: " + TriangleClass(s1, s2, s3).P().toString()
+                }else {
+                    Snackbar.make(findViewById(android.R.id.content), "Triangle does not exist", Snackbar.LENGTH_SHORT).show()
+                }
             }
         }
     }
--- app/src/main/java/deusvult/petrkamaev/homework_3/TriangleClass.kt
+++ app/src/main/java/deusvult/petrkamaev/homework_3/TriangleClass.kt
@@ -2,8 +2,8 @@ package deusvult.petrkamaev.homework_3
 
 import kotlin.math.sqrt
 
-class TriangleClass(var s1: Int = 0, var s2: Int = 0, var s3: Int = 0) {
-    var p: Double = (s1.toDouble()+s2.toDouble()+s3.toDouble())/2
+class TriangleClass(var s1: Double = 0.0, var s2: Double = 0.0, var s3: Double = 0.0) {
+    var p = (s1+s2+s3)/2
     fun S(): Double = sqrt(p*(p-s1)*(p-s2)*(p-s3))
-    fun P(): Int = s1 + s2 + s3
+    fun P(): Double = s1 + s2 + s3
 }
